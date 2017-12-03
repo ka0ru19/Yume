434d7ddeadf4f3931076889d6a7d81c716c6422d
ka0ru19 <k.prfm19@gmail.com>
2017-12-03 16:58:23 +0900

    loadのアニメを0.8秒で消えるようにした

--- Yume/ArchiveViewController.swift
+++ Yume/ArchiveViewController.swift
@@ -112,7 +112,7 @@ extension ArchiveViewController {
     
     func successLoadRecentPost(dict: Dictionary<String, AnyObject>) {
         
-        UIView.animate(withDuration: 3.0, animations: {
+        UIView.animate(withDuration: 0.8, animations: {
             self.bgImageView.alpha = 0.0
         }, completion: { _ in
             self.timer.invalidate()
